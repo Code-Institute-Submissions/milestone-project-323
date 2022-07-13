2d7739cc423118a3cfa2845d10bec5961073e6ff
EbbaStrandholm <ebba@strandholm.com>
2022-07-13 18:04:04 +0000

    Change placement of the comments for the def in game.py

--- game.py
+++ game.py
@@ -18,34 +18,33 @@ lives = 6
 #variable for game over (global)
 game_over = False
 
-
+#will return randomly chosen word for the hangman game
 def random_word():
-    #will return randomly chosen word for the hangman game
     pass
 
+#this will print out dashes where the yet to be guessed letters of the word will be
 def word_before_guess():
-    #this will print out dashes where the yet to be guessed letters of the word will be
     pass
 
+#will create a hangman in relation to how many lives are left of the player
 def create_hangman():
-    #will create a hangman in relation to how many lives are left of the player
     pass
 
+#makes sure that the player can only choose a letter that hasn't been chosen yet, and is a valid letter (i.e not a number or special case etc)
 def one_valid_letter():
-    #makes sure that the player can only choose a letter that hasn't been chosen yet, and is a valid letter (i.e not a number or special case etc)
     pass
 
+#will check if the letter guessed is correct or wrong, and update the global variables accordingly
 def guess_letter():
-    #will check if the letter guessed is correct or wrong, and update the global variables accordingly
     pass
 
+#checks if the player has won or lost the game
 def game_over():
-    #checks if the player has won or lost the game
     pass
 
+#the entry point of the game application, will call in all other mehtods for a game loop
 def game_main():
-    #the entry point of the game application, will call in all other mehtods for a game loop
     pass
 
+#will only be run if you run the game throguh the terminal or an IDE (like PyCharms)
 if _name_ == '_main_':
-    #will only be run if you run the game throguh the terminal or an IDE (like PyCharms)
