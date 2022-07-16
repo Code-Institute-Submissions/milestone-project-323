c5de407a775c29180f8993563c77ac1e28cf0e41
EbbaStrandholm <ebba@strandholm.com>
2022-07-16 08:09:42 +0000

    Add the hangman rules in a docstring above import random, and remove them from the readme.md

--- README.md
+++ README.md
@@ -1,13 +1,3 @@
----Hangman Rules---
-1. Once every game a random word will be chosen from the list in words.py
-2. The player will have a total of 6 lives, if the player chooses 6 incorrect letters he/she/they will lose.
-3. Only one letter can be guessed at a time
-4. The letter that has been previously guessed incorrectly will be shown to the player so as not to be chosen again by mistake.
-5. Before every guess the player will be shown a hangman to help keep the player on track with how many lives are left.
-6. If all the letters were correctly guessed by the player before his/hers/their lives run out the player will win the game, which
-    will be shown on screen.
-7. If all lives has been lost, the hangman drawing will be completed and the player has lost. A message will display on screen
-    telling the player of his/her/their loss.
 
 pygame has been installed
 
--- game.py
+++ game.py
@@ -1,3 +1,18 @@
+"""---Hangman Rules---
+Once every game a random word will be chosen from the list in words.py
+The player will have a total of 6 lives, if the player chooses 6 incorrect
+    letters he/she/they will lose.
+Only one letter can be guessed at a time
+The letter that has been previously guessed incorrectly will be shown to the
+    player so as not to be chosen again by mistake.
+Before every guess the player will be shown a hangman to help keep the player
+    on track with how many lives are left.
+If all the letters were correctly guessed by the player before his/hers/their
+    lives run out the player will win the game, which
+    will be shown on screen.
+If all lives has been lost, the hangman drawing will be completed and the
+    player has lost. A message will display on screen
+    telling the player of his/her/their loss."""
 import random
 import time
 
@@ -17,6 +32,7 @@ lives = 6
 
 # variable for game over (global)
 is_game_over = False
+
 
 # This fucntion makes up the methods used for the random_words generation
 
